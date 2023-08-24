d7eccc04df5085b29db11c4418aa48cf3b2b0a1e
DAbranka <david@codelaw.be>
2023-08-24 16:22:13 +0200

    Text Reveal updated

---
+++ components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1 text-9xl uppercase font-900 m-9>
+            Test Component
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    position: relative;
+    display: inline-block;
+}
+
+h1::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: black;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    animation: animate 1s forwards;
+    transition-timing-function: cubic-bezier(1, -0.04, 0.08, 1.01);
+}
+
+@keyframes animate {
+    0% {
+        height: 100%;
+    }
+    100% {
+        height: 0%;
+    }
+}
+</style>
