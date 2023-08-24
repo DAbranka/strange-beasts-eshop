6300f7c2aec348fd421c5f8e72aede5acf548af7
DAbranka <david@codelaw.be>
2023-08-24 20:14:52 +0200

    Header done - Starting Features Section

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,56 @@
 </script>
 
 <template>
-    <h1>
-        Main
-    </h1>
+    <div>
+        <!-- FEATURES SECTION -->
+        <section overflow-hidden>
+            <!-- FEATURES TITLE -->
+            <h2 text-21rem uppercase whitespace>
+                Features
+            </h2>
+            <!-- FEATURES CONTAINER -->
+            <section w-full h-screen flex justify-center >
+                <!-- FEATURES CARDS CONTAINER -->
+                <section w-7xl h-screen flex flex-row justify-between items-center>
+                    <!-- FEATURE CARD w Text -->
+                    <article w-sm h-124 flex flex-col>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="https://source.unsplash.com/random/400x400" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <p text-5xl uppercase font-bold>
+                            High quality print
+                        </p>
+                    </article>
+                    <article w-sm h-124 flex flex-col>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="https://source.unsplash.com/random/400x400" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <p text-5xl uppercase font-bold>
+                            Unique Design
+                        </p>
+                    </article>
+                    <article w-sm h-124 flex flex-col>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="https://source.unsplash.com/random/400x400" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <p text-5xl uppercase font-bold>
+                            Easy application
+                        </p>
+                    </article>
+                </section>
+            </section>
+        </section>
+    </div>
 </template>
 
 <style scoped>
-    
+    * {
+        margin: 0;
+    }
 </style>
