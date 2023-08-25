1d80eadb9068b095fcc9e7e8585a85421e665d83
DAbranka <david@codelaw.be>
2023-08-25 17:06:23 +0200

    Testimonials - Starting Cards

---
+++ components/Features.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<template>
+    <section overflow-hidden>
+            <!-- FEATURES TITLE -->
+            <div w-full>
+                <h2 text-27rem uppercase whitespace-nowrap>
+                    Features
+                </h2>
+            </div>
+            
+            <!-- FEATURES CONTAINER -->
+            <section w-full h-3xl flex justify-center >
+                <!-- FEATURES CARDS CONTAINER -->
+                <section w-7xl flex flex-row justify-between items-center>
+                    <!-- FEATURE CARD w Text -->
+
+                    <article  w-sm h-2xl flex flex-col justify-around>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="@/assets/img/features/feature01.png" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <div h-24 py-2 flex items-start>
+                            <p text-5xl uppercase font-bold>
+                                High Quality Print
+                            </p>
+                        </div>
+                    </article>
+
+                    <article  w-sm h-2xl flex flex-col justify-around>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="@/assets/img/features/feature02.png" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <div h-24 py-2 flex items-start>
+                            <p text-5xl uppercase font-bold>
+                                Unique <br> Design
+                            </p>
+                        </div>
+                    </article>
+
+                    <article  w-sm h-2xl flex flex-col justify-around>
+                        <!-- IMAGE -->
+                        <div flex items-center justify-center overflow-hidden rounded-2xl>
+                            <img src="@/assets/img/features/feature03.png" alt="image/png">	
+                        </div>
+                        <!-- TEXT -->
+                        <div h-24 py-2 flex items-start>
+                            <p text-5xl uppercase font-bold>
+                                Easy Aplication
+                            </p>
+                        </div>
+                    </article>
+
+                </section>
+            </section>
+        </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/Testimonials.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+    <section w-full h-lg >
+            <!-- TITLE -->
+            <div w-full>
+                <h3 text-56 uppercase>
+                    They say...
+                </h3>
+            </div>
+            <!-- TESTIMONIALS CONTAINER -->
+            <div w-full bg-black>
+                <!-- TESTIMONIAL CARD -->
+                <article bg-white w-lg>
+                    <!-- CONTENT -->
+                    <section w-sm>
+                        <!-- IMAGE -->
+                    <div>
+                        <img src="" alt="">
+                    </div>
+
+                    <!-- TEXT -->
+                    <section>
+                        <!-- Comment -->
+                        <article>
+                            <p>
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae provident ipsa vero iusto rerum quidem! Voluptates ratione blanditiis velit odio aliquid, vero eius, nam nobis est consequuntur, eligendi doloribus!"
+                            </p>
+                        </article>
+                        <article flex flex-row w-full justify-between>
+                            <!-- Author -->
+                            <p text-prime font-800>
+                                Customer Name
+                            </p>
+                            <!-- Rating -->
+                            <p>
+                                ★★★★★
+                            </p>
+                        </article>
+                    </section>
+                    </section>
+                </article>
+            </div>
+        </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
