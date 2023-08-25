7670fe69dac4d5b5bed4ef184ca8fd25ec433155
DAbranka <david@codelaw.be>
2023-08-25 18:11:35 +0200

    Testimonials - Cards with warousel done

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -4,46 +4,21 @@
 
 <template>
     <section w-full h-lg >
-            <!-- TITLE -->
-            <div w-full>
-                <h3 text-56 uppercase>
-                    They say...
-                </h3>
-            </div>
-            <!-- TESTIMONIALS CONTAINER -->
-            <div w-full bg-black>
-                <!-- TESTIMONIAL CARD -->
-                <article bg-white w-lg>
-                    <!-- CONTENT -->
-                    <section w-sm>
-                        <!-- IMAGE -->
-                    <div>
-                        <img src="" alt="">
-                    </div>
 
-                    <!-- TEXT -->
-                    <section>
-                        <!-- Comment -->
-                        <article>
-                            <p>
-                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae provident ipsa vero iusto rerum quidem! Voluptates ratione blanditiis velit odio aliquid, vero eius, nam nobis est consequuntur, eligendi doloribus!"
-                            </p>
-                        </article>
-                        <article flex flex-row w-full justify-between>
-                            <!-- Author -->
-                            <p text-prime font-800>
-                                Customer Name
-                            </p>
-                            <!-- Rating -->
-                            <p>
-                                ★★★★★
-                            </p>
-                        </article>
-                    </section>
-                    </section>
-                </article>
-            </div>
-        </section>
+        <!-- TITLE -->
+        <div w-full>
+            <h3 text-56 uppercase>
+                They say...
+            </h3>
+        </div>
+
+        <!-- TESTIMONIALS CONTAINER -->
+        <article w-full bg-black py-9>
+            <!-- TESTIMONIAL CARD -->
+            <TestimonialsCards />
+        </article>
+
+    </section>
 </template>
 
 <style lang="scss" scoped>
---
+++ components/TestimonialsCards.vue
@@ -0,0 +1,49 @@
+
+<script setup>
+
+</script>
+<template>
+    <div class="cards">
+        <div class="slide" flex flex-row>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+        </div>
+        <div class="slide" flex flex-row>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+            <TestimonialsCard mx-6/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .cards {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        overflow: hidden;
+    }
+    .slide {
+        display: flex;
+        flex-direction: row;
+        animation: slide 50s linear infinite;
+    }
+    @keyframes slide {
+        0% {
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+
+    .cards:hover .slide{
+        animation-play-state: paused;
+    }
+</style>
