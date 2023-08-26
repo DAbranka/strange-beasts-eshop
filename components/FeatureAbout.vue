1febd7c963c43540567c714840a48ca10829da84
DAbranka <david@codelaw.be>
2023-08-26 13:10:18 +0200

    About section - updated

--- components/FeatureAbout.vue
+++ components/FeatureAbout.vue
@@ -9,18 +9,32 @@
             <p uppercase text-42 font-900>
                 About <br> the artist
             </p>
-            <!-- IMG / TEXT -->
-            <section flex flex-row>
-                <!-- IMG -->
-                <div w-full h-3xl relative overflow-hidden>
-                    <img  absolute top--10 left--60 src="@/assets/img/logo.png" alt="image/png">
-                </div>
-                <!-- TEXT -->
-                <article w-full flex items-end p-9>
-                    <p text-xl>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, deserunt consectetur omnis non veniam voluptatum, excepturi inventore ipsam dicta autem aspernatur placeat atque? Consequatur nulla repellat quam veniam necessitatibus rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro ex veritatis accusantium provident, culpa cupiditate asperiores illum facere et eos ipsum recusandae, ea quaerat? Quia doloremque quasi similique iste.
-                    </p>
-                </article>
+
+            <!-- ABOUT CONTAINER -->
+            <section overflow-hidden>
+
+                <!-- CONTENU -->
+                <section max-w-7xl flex flex-row mx-auto>
+
+                    <!-- LEFT -->
+                    <section w-full h-full>
+                        <!-- IMG -->
+                        <div w-full h-3xl>
+                            <img w-full src="@/assets/img/logo.png" alt="image/png">
+                        </div>
+                    </section>
+
+                    <!-- RIGHT -->
+                    <section w-full flex items-end>
+                        <!-- TEXT -->
+                        <article p-9>
+                            <p text-xl>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, deserunt consectetur omnis non veniam voluptatum, excepturi inventore ipsam dicta autem aspernatur placeat atque? Consequatur nulla repellat quam veniam necessitatibus rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro ex veritatis accusantium provident, culpa cupiditate asperiores illum facere et eos ipsum recusandae, ea quaerat? Quia doloremque quasi similique iste.
+                            </p>
+                        </article>
+                    </section>
+                    
+                </section>
             </section>
         </section>
     </div>
