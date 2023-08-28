35043dbf8837189126dd5c91b7afa5b8672eacc7
DAbranka <d.abranka@hotmail.com>
2023-08-28 02:39:35 +0200

    Changed the id of products to manually ids - all data fetched except image that doesn't work

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,18 +1,30 @@
-<script setup>
-    const { id } = useRoute().params
-    const { products } = useProducts()
-</script>
-
 <template>
     <div>
         <section>
-            <p>
-                {{ id }}
-            </p>
+            <div v-if="product">
+                <h2>{{ product.title }}</h2>
+                <p>Price: {{ product.price }}</p>
+                <img :src="product.image" :alt="product.title" />
+            </div>
+            <div v-else>
+                <p>Product not found</p>
+            </div>
         </section>
     </div>
-</template>
+    </template>
+    
+    <script setup>
+        const { id } = useRoute().params
+        const { products } = useProducts()
 
-<style scoped>
+        
+        console.log('Route ID:', id)
+        console.log('All Products:', products.value)
 
-</style>
+        const product = products.value.find((p) => p.id === id)
+        console.log('Selected Product:', product)
+    </script>
+
+    <style scoped>
+  /* Vos styles ici */
+    </style>
