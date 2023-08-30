02826e3b16f73f482b7c0c65b62df9b3eef4284b
DAbranka <david@codelaw.be>
2023-08-30 23:23:58 +0200

    Latest section showing the latest products

--- components/LatestProducts.vue
+++ components/LatestProducts.vue
@@ -1,10 +1,11 @@
 <script setup>
-    const { stickers } = useStickers()
+    const { products } = useProducts()
 </script>
 
 <template>
     <div>
         <section >
+            <!--  TITLE -->
             <div class="title" mb-24>
                 <div class="slide">
                     <h3 text-56 font-900 uppercase whitespace-nowrap overflow-hidden mt-24>
@@ -21,85 +22,17 @@
             <!-- PRODUCTS CONTAINER -->
             <section w-full overflow-hidden>
                 <!-- Contenu -->
-                <section mx-auto max-w-7xl flex flex-row justify-around>
+                <article mx-auto max-w-7xl flex flex-row justify-around>
                     <!-- Card -->
-
-                    <article>
-                        <!-- Image -->
-                        <section productCard hover:drop-shadow-2xl mb-3>
-                            <img :src="stickers[1][0].image" alt="stickers[1][0].title">
-                        </section>
-
-                        <!-- Card Title -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold uppercase">{{ stickers[1][0].title }}</h4>
-                        </section>
-
-                        <!-- Card Price -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold">Price: €{{ stickers[1][0].price }}</h4>
-                        </section>
-
-                        <!-- Add to cart -->
-                        <section class="text-center">
-                            <button addCartBtn hover:drop-shadow-xl>
-                                Buy for €{{ stickers[1][0].price }}
-                            </button>
-                        </section>
-                    </article>
-
-                    <article>
-                        <!-- Image -->
-                        <section productCard hover:drop-shadow-2xl mb-3>
-                            <img :src="stickers[0][3].image" alt="stickers[0][3].title">
-                        </section>
-
-                        <!-- Card Title -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold uppercase">{{ stickers[0][3].title }}</h4>
-                        </section>
-
-                        <!-- Card Price -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold">Price: €{{ stickers[0][3].price }}</h4>
-                        </section>
-
-                        <!-- View Product -->
-                        <section class="text-center">
-                            <button addCartBtn hover:drop-shadow-xl>
-                                Buy for €{{ stickers[0][3].price }}
-                            </button>
-                        </section>
-                    </article>
-
-                    <article>
-                        <!-- Image -->
-                        <section productCard hover:drop-shadow-2xl mb-3>
-                            <img :src="stickers[1][2].image" alt="stickers[1][2].title">
-                        </section>
-
-                        <!-- Card Title -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold uppercase">{{ stickers[1][2].title }}</h4>
-                        </section>
-
-                        <!-- Card Price -->
-                        <section class="text-center mb-3">
-                            <h4 class="text-2xl font-bold">Price: €{{ stickers[1][2].price }}</h4>
-                        </section>
-
-                        <!-- View Product -->
-                        <section class="text-center">
-                            <button addCartBtn hover:drop-shadow-xl>
-                                Buy for €{{ stickers[1][2].price }}
-                            </button>
-                        </section>
-                    </article>
-
-                </section>
+                    <LatestProductsCard
+                        v-for="(product, index) in products.slice(-3)" 
+                        :key="index"
+                        :product="product"
+                    />
+                </article>
             </section>
 
-            <!-- Button -->
+            <!-- ALL PRODUCTS BTN -->
             <section class="text-center mt-24">
                 <nuxt-link to="/products">
                     <button bg-gray hover:bg-prime text-white border-none text-2xl font-bold py-2 px-6 uppercase rounded-xl cursor-pointer>
---
+++ components/LatestProductsCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const { product } = defineProps(['product'])
+</script>
+
+<template>
+    <div>
+        <article>
+            <!-- Image -->
+            <section productCard mb-3>
+                <img w-74 :src="product.image" alt="product.title">
+            </section>
+            <!-- Card Title -->
+            <section class="text-center mb-3">
+                <h4 class="text-2xl font-bold uppercase">{{ product.title }}</h4>
+            </section>
+            <!-- Add to cart -->
+            <section class="text-center">
+                <button addCartBtn hover:drop-shadow-xl>
+                    Buy for €{{ product.price }}
+                </button>
+            </section>
+        </article>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
