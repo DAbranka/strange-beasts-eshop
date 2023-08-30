f412716bd33e414f25328a5da867b5e8535f86d9
DAbranka <david@codelaw.be>
2023-08-30 17:48:53 +0200

    Listed Cart Products - style updaded

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -4,22 +4,38 @@
 </script>
 
 <template>
-    <div>
-        <section grid grid-cols-1 grid-rows-2 gap-10>
+    <div w-full>
+        <section mx-auto max-w-7xl>
             <div>
-                <h1 text-42>
-                    Cart Page
+                <h1 text-42 m-10>
+                    Cart
                 </h1>
             </div>
             <div>
                 <section>
-                    <p v-for="product in addedProducts" :key="product.id">
-                        Product ID: {{ product.id }}
-                        Product Title: {{ product.title }}
-                        Product Price: {{ product.price }}
-                    </p>
+                    <article v-for="product in addedProducts" :key="product.id">
+                        <section class="product" p-10 flex flex-row items-center>
+                            <div productCard bg-white mr-10>
+                                <img w-74 :src="product.image" alt="">
+                            </div>
+                            <div>
+                                <h2 text-7xl font-900 mb-10>
+                                    {{ product.title }}
+                                </h2>
+                                <p text-5xl>
+                                    €{{ product.price }}
+                                </p>
+                            </div>
+                        </section>
+                    </article>
                 </section>
             </div>
         </section>
     </div>
 </template>
+
+<style scoped>
+    .product {
+        border-top: 1px solid #E5E5E5;
+    }
+</style>
